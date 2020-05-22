fede4c9dd1dfb979b8d41955b8bca4a8fe2666cc
Ritik <ritik.kumar006@gmail.com>
2020-05-22 15:04:15 +0530

    Updated User associations for comments and campgrounds

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -15,11 +15,15 @@ router.get("/campgrounds", function(req, res){
     });
 });
 
-router.post("/campgrounds", function(req, res){
+router.post("/campgrounds", isLoggedIn, function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
-   var newCampground = {name: name, image: image, description: description};
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    };
+   var newCampground = {name: name, image: image, description: description, author: author};
    Campground.create(newCampground, function(err, newCamp){
         if(err){
             console.log(err);
@@ -31,7 +35,7 @@ router.post("/campgrounds", function(req, res){
 
 });
 
-router.get("/campgrounds/new", function(req, res){
+router.get("/campgrounds/new", isLoggedIn, function(req, res){
     res.render("./campgrounds/new"); 
 });
 
--- routes/comments.js
+++ routes/comments.js
@@ -26,6 +26,9 @@ router.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
                     console.log(err);
                 }
                 else{
+                    comment.author.id = req.user._id;
+                    comment.author.username = req.user.username;
+                    comment.save();
                     campground.comments.push(comment);
                     campground.save();
                     res.redirect("/campgrounds/" + req.params.id)
