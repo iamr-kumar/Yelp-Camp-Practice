4020081f19ea632d32b87ab630e0f40e054cb6a6
Ritik Kumar <ritik.kumar006@gmail.com>
2020-06-26 04:04:10 +0530

    Updated User Model

--- models/user.js
+++ models/user.js
@@ -2,10 +2,28 @@ const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
 
 const UserSchema = new mongoose.Schema({
-    username: String,
-    password: String
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    }
 });
 
-UserSchema.plugin(passportLocalMongoose);
+UserSchema.plugin(passportLocalMongoose, {usernameQueryFields: ['email']});
 
 module.exports = mongoose.model("User", UserSchema);
